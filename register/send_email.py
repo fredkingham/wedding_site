cdaaa998be19ffa1b0972eaba8826bca29faefa5
fredkingham <fredkingham@gmail.com>
2018-03-31 17:36:15 +0100

    email template tweaks

--- register/send_email.py
+++ register/send_email.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.template.loader import get_template
+from sesame import utils
 
 SUBJECT = "Christina & Fred's wedding - 1 September"
 FROM_EMAIL = "christinaandfredding@gmail.com"
@@ -7,7 +8,8 @@ FROM_EMAIL = "christinaandfredding@gmail.com"
 
 def send_invite(user):
     tmp = get_template("email_template")
-    content = tmp.render(user)
+    params = utils.get_query_string(user)
+    content = tmp.render(dict(user=user, params=params))
     send_mail(
         SUBJECT,
         content,
--- register/urls.py
+++ register/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 from register import views
 from register import api
 
 urlpatterns = [
-    path('logout/', auth_views.logout, name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.HomeView.as_view(), name="home"),
-    path('login/', views.Login.as_view(), name="login"),
+    path('rsvp/', views.Login.as_view(), name="rsvp"),
     path('api/', include(api.router.urls)),
 ]
